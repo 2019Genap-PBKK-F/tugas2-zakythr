41471b22078651a35fce6c796f2f693ae56e7f30
Zaky Thariq H <zakythariq@gmail.com>
2020-02-20 13:51:09 +0700

    first

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express('10.199.14.46')
-const port = 8026
+const http = require("http");
+const hostname = '10.199.14.46';
+const port = 8026;
 
-app.get('/', (req, res) => res.send('Hello Zaky!'))
+//Create HTTP server and listen on port 8015 for requests
+const server = http.createServer((req, res) => {
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Test\n');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
